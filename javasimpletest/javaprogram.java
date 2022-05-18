0a8b879a45bd4f46b4a8ec6c56514efea5fab414
lakshmi <lakshmipraveena.nalluri@gmail.com>
2022-05-18 12:00:32 -0500

    hello this is my 1st javaprogram

---
+++ javasimpletest/javaprogram.java
@@ -0,0 +1,16 @@
+package javasimpletest;
+
+public class javaprogram {
+
+	public static void main(String[] args) {
+		String s="hello there";
+		String remove="llo";
+		String act= s.replaceAll(remove, "");
+		System.out.println(act.trim());
+
+	}
+
+
+	}
+
+
